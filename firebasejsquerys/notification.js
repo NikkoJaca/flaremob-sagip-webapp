6abfc6cc830a1ecf52aa9eea5e1e1b5889fab864
Michael Cembrano <stmicheal77@gmail.com>
2019-03-04 15:41:30 +0800

    Update for notifications 4

--- firebasejsquerys/notification.js
+++ firebasejsquerys/notification.js
@@ -2,19 +2,20 @@ const dbRef = firebase.database().ref();
 const usersRef = dbRef.child('tblDonatedItems');
 const moneyRef = dbRef.child('tblDonatedMoney');
 let adminnotseen = 0;
-usersRef.on("child_added",snap=>{
+usersRef.once("child_added",snap=>{
     if(snap.child("isAdminNotified").val()==="0"){
-        adminnotseen+=1;
-
+        adminnotseen = adminnotseen+=1;
+        document.getElementById('notifcount').innerText = adminnotseen;
+        document.getElementById('notifcount1').innerText = adminnotseen;
         console.log(snap.key)
     }else if(snap.child("isAdminNotified").val()==="1"){
 
     }
 
 });
-moneyRef.on("child_added",snap=>{
+moneyRef.once("child_added",snap=>{
     if(snap.child("isAdminNotified").val()==="0"){
-        adminnotseen+=1;
+        adminnotseen = adminnotseen+=1;
         document.getElementById('notifcount').innerText = adminnotseen;
         document.getElementById('notifcount1').innerText = adminnotseen;
         console.log(snap.key);
